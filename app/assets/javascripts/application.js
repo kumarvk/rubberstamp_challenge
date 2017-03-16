28ddddeb2b549934fb23a32a00980cb729fa04ce
Vipul <kumarvk.pool@gmail.com>
2017-03-16 13:36:25 +0530

    add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.dataTables.min
+//= require bootstrap-sprockets
+//= require bootstrap-data-tables
 //= require turbolinks
 //= require_tree .
